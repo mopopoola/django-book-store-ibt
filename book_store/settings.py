f2728112ceab61ab977636e51cd3ab374ddae5f8
mopopoola <78879877+mopopoola@users.noreply.github.com>
2023-11-05 00:00:45 +0000

    Update settings.py

--- book_store/settings.py
+++ book_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-419+ims(b(tca!&gqj8byqi*k!_jmk(tejejj2cv%q@2w=73ju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
